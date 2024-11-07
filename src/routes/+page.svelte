f8770a7439e15213ce4ce2d25a861cd3cb438600
T2 <studioTeaTwo@gmail.com>
2024-11-08 01:02:05 +0900

    respond events

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,16 +16,32 @@
 
   let eventData = ""
 
+  let convertedKey = ""
+
   onMount(async() => {
     console.log(window.nostr)
-    const pubkey = await window.nostr.getPublicKey()
-    npub = encodeNpub(pubkey)
 
-    window.addEventListener("nostr:accountchanged", (e) => {
+    try {
+      const pubkey = await window.nostr.getPublicKey()
+      npub = encodeToNpub(pubkey)
+    } catch(e) {
+      // If you disables the prefs, i.e. Enable, Use built-in NIP07 or Use TrustedSites, then come here.
+      alert(`NIP-07 is disabled! detail:${e}`)
+    }
+
+    window.nostr.addEventListener("accountChanged", (e) => {
+      // If you trun on the "Notify Account Changed", then come here.
+
       console.log(e)
       prevNpub = npub
-      npub = encodeNpub(e.detail)
+      npub = encodeToNpub(e.detail)
       npubChanged = true
+      alert(`Account Changed! detail:${e}`)
+    })
+    window.addEventListener("providerChanged", (e) => {
+      // If you enables the prefs, i.e. Enable, Use built-in NIP07, then come here.
+      console.log(e)
+      alert(`Provider Changed! detail:${e}`)
     })
   })
 
@@ -38,7 +54,12 @@
       content: kind1Text,
       created_at: kind1CreatedAt === 0 ? Date.now() : kind1CreatedAt,
     }
-    issuedEvent = await window.nostr.signEvent(nevent)
+    try {
+      issuedEvent = await window.nostr.signEvent(nevent)
+    } catch (e) {
+      // If you disables the prefs, then dipaly the reason.
+      alert(`You can't! detail:${e}`)
+    }
   }
 
   const onClickVerify = (e: MouseEvent & {
@@ -48,16 +69,26 @@
 
     let pubkey = event.pubkey
     if (event.pubkey.startsWith("npub")) {
-      const {prefix, words} = bech32.decode(pubkey, Bech32MaxSize)
-      pubkey = bytesToHex(new Uint8Array(bech32.fromWords(words)))
+      pubkey = decodeNpub(pubkey)
     }
     const result = schnorr.verify(event.sig, event.id, pubkey)
     alert(`They are ${result}`)
   }
 
-  function encodeNpub(pubkey) {
+  const onChangePubConvert = (e: Event & {
+    currentTarget: EventTarget & HTMLInputElement;
+  }) => {
+    const key = e.target.value
+    convertedKey = key.startsWith("npub") ? decodeNpub(key) : encodeToNpub(key)
+  }
+
+  function encodeToNpub(pubkey) {
     const words = bech32.toWords(hexToBytes(pubkey))
     return bech32.encode('npub', words, Bech32MaxSize)
+  }
+  function decodeNpub(npub) {
+    const {prefix, words} = bech32.decode(npub, Bech32MaxSize)
+    return bytesToHex(new Uint8Array(bech32.fromWords(words)))
   }
 </script>
 
@@ -73,11 +104,11 @@
   {/if}
 
   <h2>kind1</h2>
-  <div class="kind1-input">
+  <div class="flex-row">
     <input type="text" bind:value={kind1Text} placeholder="text" />
     <button on:click={onClickIssueEvent}>Issue</button>
   </div>
-  <div class="kind1-input">
+  <div class="flex-row">
     <label>debug</label>
     <input type="number" bind:value={kind1CreatedAt} placeholder="created_at" />
   </div>
@@ -86,13 +117,20 @@
   {/if}
 
   <h2>debug</h2>
-  <div class="kind1-input">
+  <div class="flex-row">
     <textarea
       style="min-width: 300px; min-height: 200px; field-sizing: content;"
       placeholder="event data"
       bind:value={eventData} />
     <button on:click={onClickVerify}>Verify</button>
   </div>
+  <div class="flex-col">
+    <input type="text" placeholder="npub or pub" on:change={onChangePubConvert} />
+    {#if convertedKey }
+    <p class="kind1-event">{convertedKey}</p>
+    {/if}
+  </div>
+
 </section>
 
 <style>
@@ -101,9 +139,15 @@
     margin: auto;
   }
 
-  .kind1-input {
+  .flex-row {
     display: flex;
     flex-direction: row;
+    gap: 1px;
+    margin-bottom: 10px;
+  }
+  .flex-col {
+    display: flex;
+    flex-direction: column;
     gap: 1px;
     margin-bottom: 10px;
   }
